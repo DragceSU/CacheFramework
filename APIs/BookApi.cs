f0115533e0c2b36a15b5ea42c73627bc2cb57827
Dragan Shutinovski <dragan.sutinovski@gmail.com>
2015-12-14 08:22:56 +0100

    CachingFramework
    EasyCache

---
+++ APIs/BookApi.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using Cache.Caching;
+using EasyCaching.Model;
+
+namespace EasyCaching.APIs
+{
+    public class BookApi
+    {
+        [CacheableResult]
+        public static IEnumerable<Book> GetBooks(string authorName)
+        {
+            Thread.Sleep(1000);
+            using (var repository = new BookRepository())
+            {
+                //return repository.Authors
+                //                 .Where(a => a.Name == authorName)
+                //                 .SelectMany(a => a.Books)
+                //                 .ToList();
+                return new List<Book>
+                {
+                    new Book {Id = 1, Title = "Life Of Brian", Authors = {new Author {Id = 1, Name = "Ross L. Finney"}}}
+                };
+            }
+        }
+
+        [AffectedCacheableMethods("EasyCaching.APIs.BookApi.GetBooks")]
+        public static void ChangeAuthorName(string oldName, string newName)
+        {
+            using (var repository = new BookRepository())
+            {
+                // Commenting this to avoid making actual changes, or breaking the code
+                // if the author doesn't exist in the database.
+
+                /*repository.Authors.Single(a => a.Name == oldName).Name = newName;
+                repository.SaveChanges();*/
+            }
+        }
+    }
+}
